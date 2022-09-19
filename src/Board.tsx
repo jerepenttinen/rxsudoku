cb9d285e74e9aea9998c5060a07405902f10b38d
Jere Penttinen <jpenttinen@tuta.io>
2022-09-19 08:34:33 +0300

    Cleanup redundant styles

--- src/Board.tsx
+++ src/Board.tsx
@@ -23,7 +23,7 @@ function Clickable({ cell, number }: { cell: string; number: number }) {
         mark
           ? "text-zinc-700 dark:text-zinc-300"
           : "text-transparent hover:text-stone-700/60 dark:hover:text-stone-300/60"
-      } aspect-square h-full w-full hover:shadow-[inset_0em_0em_0em_10em_rgba(0,0,0,0.12)] dark:hover:shadow-[inset_0em_0em_0em_10em_rgba(1,1,1,0.12)]`}
+      } h-full w-full hover:shadow-[inset_0em_0em_0em_10em_rgba(0,0,0,0.12)] dark:hover:shadow-[inset_0em_0em_0em_10em_rgba(1,1,1,0.12)]`}
       onClick={handleClick}
       onDoubleClick={handleDoubleClick}
     >
@@ -52,7 +52,7 @@ function Cell({ cell }: { cell: string }) {
       onClick={() => setCurrentCell(cell)}
       className={`${
         isCurrent && "z-10 ring-2 ring-inset ring-blue-500"
-      }  flex aspect-square items-center justify-center outline outline-1 outline-zinc-900 dark:outline-zinc-600`}
+      }  flex items-center justify-center outline outline-1 outline-zinc-900 dark:outline-zinc-600`}
     >
       {digit !== "0" ? (
         <span
@@ -64,7 +64,7 @@ function Cell({ cell }: { cell: string }) {
         </span>
       ) : (
         <div
-          className={`grid aspect-square h-full w-full grid-cols-3 grid-rows-3 gap-0 p-1 ${
+          className={`grid h-full w-full grid-cols-3 grid-rows-3 p-1 ${
             highlighted && "bg-blue-300/80 dark:bg-blue-900/30"
           }
           ${isCurrent && "z-10 ring-2 ring-inset ring-blue-500"}
@@ -103,12 +103,10 @@ const subGrids = [
 
 function Board() {
   return (
-    <div className="aspect-square bg-zinc-50 drop-shadow-2xl dark:bg-zinc-800">
-      <div className="grid h-[80vmin] w-[80vmin] grid-cols-3 grid-rows-3 text-center">
-        {subGrids.map((s, i) => (
-          <SubGrid key={"S" + i} list={s} />
-        ))}
-      </div>
+    <div className="grid h-[80vmin] w-[80vmin] grid-cols-3 grid-rows-3 bg-zinc-50 text-center drop-shadow-2xl dark:bg-zinc-800">
+      {subGrids.map((s, i) => (
+        <SubGrid key={"S" + i} list={s} />
+      ))}
     </div>
   );
 }
