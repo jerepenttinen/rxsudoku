61d0203dfd19b978097c3b138f36a79a441b62a1
Jere Penttinen <jere@disroot.org>
2023-02-07 19:19:57 +0200

    Style highlight buttons

--- ui/src/HighlightButtons.tsx
+++ ui/src/HighlightButtons.tsx
@@ -1,17 +1,22 @@
 import { For } from "solid-js";
 import { sudoku } from "./sudoku";
+import clsx from "clsx";
 
 function HighlightButtons() {
   const context = sudoku.state.context;
   return (
-    <div class="my-4 flex select-none flex-row">
+    <div class="my-4 inline-flex select-none" role="group">
       <For each={[1, 2, 3, 4, 5, 6, 7, 8, 9]}>
         {(num) => (
           <button
-            class={`rounded-none p-1 ${
-              num === context.highlight &&
-              "bg-blue-500 hover:bg-blue-400 active:bg-blue-500 dark:bg-blue-500 dark:text-zinc-900 dark:hover:bg-blue-400 dark:active:bg-blue-500"
-            }`}
+            type="button"
+            class={clsx(
+              "border-t border-b border-r border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 first:rounded-l-lg last:rounded-r-lg hover:bg-gray-100 hover:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white",
+              {
+                "z-10 text-blue-700 ring-2 ring-blue-700 dark:text-white dark:ring-blue-500":
+                  context.highlight === num,
+              },
+            )}
             onClick={() => sudoku.send({ type: "HIGHLIGHT", digit: num })}
           >
             {num}
