4e8000088fc6ffa99142e2786bf9e8ed9436c4a4
Jere Penttinen <jere@disroot.org>
2023-02-11 20:32:32 +0200

    Add wasm dir

---
+++ wasm/src/lib.rs
@@ -0,0 +1,77 @@
+mod utils;
+
+use utils::set_panic_hook;
+use wasm_bindgen::prelude::*;
+
+use serde::{Deserialize, Serialize};
+use sudoku::strategy::Strategy;
+use sudoku::strategy::StrategySolver;
+use sudoku::Sudoku;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+const STRATEGIES: &[Strategy] = &[
+    // difficulty as assigned by
+    // SudokuExplainer
+    Strategy::NakedSingles,     // 23
+    Strategy::HiddenSingles,    // 15
+    Strategy::LockedCandidates, // 28
+    Strategy::NakedPairs,       // 30
+    Strategy::XWing,            // 32
+    Strategy::HiddenPairs,      // 34
+    Strategy::NakedTriples,     // 36
+    Strategy::Swordfish,        // 38
+    Strategy::HiddenTriples,    // 40
+    Strategy::NakedQuads,       // 50
+    Strategy::Jellyfish,        // 52
+    Strategy::HiddenQuads,      // 54
+];
+
+#[derive(Serialize, Deserialize)]
+struct Grid {
+    grid: String,
+    difficulty: i32,
+}
+
+#[wasm_bindgen]
+pub fn generate_grid() -> String {
+    set_panic_hook();
+    loop {
+        let sudoku = Sudoku::generate();
+
+        let solver = StrategySolver::from_sudoku(sudoku);
+
+        if let Ok((_, deductions)) = solver.solve(&STRATEGIES) {
+            // let grid = Grid {
+            //     grid: ,
+            //     difficulty: deductions.iter().map(|d| score_strat(d.strategy())).sum(),
+            // };
+
+            let line: &str = &sudoku.to_str_line();
+
+            return line.into();
+        }
+    }
+}
+
+fn score_strat(strategy: Strategy) -> i32 {
+    return match strategy {
+        Strategy::NakedSingles => 23,
+        Strategy::HiddenSingles => 15,
+        Strategy::LockedCandidates => 28,
+        Strategy::NakedPairs => 30,
+        Strategy::NakedTriples => 36,
+        Strategy::NakedQuads => 50,
+        Strategy::HiddenPairs => 34,
+        Strategy::HiddenTriples => 40,
+        Strategy::HiddenQuads => 54,
+        Strategy::XWing => 32,
+        Strategy::Swordfish => 38,
+        Strategy::Jellyfish => 52,
+        _ => todo!(),
+    };
+}
