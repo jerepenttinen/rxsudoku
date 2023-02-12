17fb9a9604efb324fdae64d12b77852d2e9baa1a
Jere Penttinen <jere@disroot.org>
2023-02-12 16:01:20 +0200

    Fix highlight with keyboard

--- src/withHotkeys.tsx
+++ src/withHotkeys.tsx
@@ -1,10 +1,18 @@
-import { createShortcut } from "@solid-primitives/keyboard";
+import { createShortcut, useKeyDownList } from "./keyboard";
 import { sudoku } from "./sudoku";
-import { Component } from "solid-js";
+import { Component, createEffect } from "solid-js";
 
 export default function withHotkeys(wrapped: Component) {
   for (let i = 1; i <= 9; i++) {
-    createShortcut(["Control", i.toString()], () =>
+    const key = "Digit" + i.toString();
+    createShortcut(["ControlLeft", key], () =>
+      sudoku.send({
+        type: "TOGGLEMARK",
+        cell: sudoku.state.context.cursor,
+        mark: i,
+      }),
+    );
+    createShortcut(["ControlRight", key], () =>
       sudoku.send({
         type: "TOGGLEMARK",
         cell: sudoku.state.context.cursor,
@@ -12,15 +20,20 @@ export default function withHotkeys(wrapped: Component) {
       }),
     );
 
-    // TODO: doesn't work, due to the fact that pressed button might be the special character behind number
-    createShortcut(["Shift", i.toString()], () => {
+    createShortcut(["ShiftLeft", key], () => {
+      sudoku.send({
+        type: "HIGHLIGHT",
+        digit: i,
+      });
+    });
+    createShortcut(["ShiftRight", key], () => {
       sudoku.send({
         type: "HIGHLIGHT",
         digit: i,
       });
     });
 
-    createShortcut([i.toString()], () =>
+    createShortcut([key], () =>
       sudoku.send({
         type: "SETCELL",
         cell: sudoku.state.context.cursor,
@@ -33,35 +46,47 @@ export default function withHotkeys(wrapped: Component) {
   createShortcut(["ArrowUp"], () =>
     sudoku.send({ type: "MOVECURSOR", direction: "up" }),
   );
-  createShortcut(["Control", "ArrowUp"], () =>
+  createShortcut(["ControlLeft", "ArrowUp"], () =>
+    sudoku.send({ type: "MOVECURSOR", direction: "up", subgrid: true }),
+  );
+  createShortcut(["ControlRight", "ArrowUp"], () =>
     sudoku.send({ type: "MOVECURSOR", direction: "up", subgrid: true }),
   );
 
   createShortcut(["ArrowDown"], () =>
     sudoku.send({ type: "MOVECURSOR", direction: "down" }),
   );
-  createShortcut(["Control", "ArrowDown"], () =>
+  createShortcut(["ControlLeft", "ArrowDown"], () =>
+    sudoku.send({ type: "MOVECURSOR", direction: "down", subgrid: true }),
+  );
+  createShortcut(["ControlRight", "ArrowDown"], () =>
     sudoku.send({ type: "MOVECURSOR", direction: "down", subgrid: true }),
   );
 
   createShortcut(["ArrowLeft"], () =>
     sudoku.send({ type: "MOVECURSOR", direction: "left" }),
   );
-  createShortcut(["Control", "ArrowLeft"], () =>
+  createShortcut(["ControlLeft", "ArrowLeft"], () =>
+    sudoku.send({ type: "MOVECURSOR", direction: "left", subgrid: true }),
+  );
+  createShortcut(["ControlRight", "ArrowLeft"], () =>
     sudoku.send({ type: "MOVECURSOR", direction: "left", subgrid: true }),
   );
 
   createShortcut(["ArrowRight"], () =>
     sudoku.send({ type: "MOVECURSOR", direction: "right" }),
   );
-  createShortcut(["Control", "ArrowRight"], () =>
+  createShortcut(["ControlLeft", "ArrowRight"], () =>
+    sudoku.send({ type: "MOVECURSOR", direction: "right", subgrid: true }),
+  );
+  createShortcut(["ControlRight", "ArrowRight"], () =>
     sudoku.send({ type: "MOVECURSOR", direction: "right", subgrid: true }),
   );
 
-  createShortcut(["U"], () => sudoku.send({ type: "UNDO" }));
-  createShortcut(["R"], () => sudoku.send({ type: "REDO" }));
+  createShortcut(["KeyU"], () => sudoku.send({ type: "UNDO" }));
+  createShortcut(["KeyR"], () => sudoku.send({ type: "REDO" }));
 
-  createShortcut(["T"], () => {
+  createShortcut(["KeyT"], () => {
     sudoku.send({
       type: "TOGGLEMARK",
       cell: sudoku.state.context.cursor,
@@ -69,12 +94,17 @@ export default function withHotkeys(wrapped: Component) {
     });
   });
 
-  createShortcut(["F"], () => {
+  createShortcut(["KeyF"], () => {
     sudoku.send({
       type: "SETCELL",
       cell: sudoku.state.context.cursor,
       digit: sudoku.state.context.highlight,
     });
+  });
+
+  const keys = useKeyDownList();
+  createEffect(() => {
+    console.log(keys[0]());
   });
 
   return wrapped;
