24bc5f55e47e896ef2b5c45b25e54fe8854cdc77
Jere Penttinen <jere@disroot.org>
2023-03-07 09:54:18 +0200

    Make highlight buttons bigger, layouting

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import DifficultyButtons from "./components/DifficultyButtons";
 
 function App() {
   return (
-    <div class="flex h-full min-h-screen flex-col items-center py-10 dark:bg-gray-900">
-      <div class="flex w-full flex-col md:max-w-screen-md">
-        <DifficultyButtons />
+    <div class="flex h-full min-h-screen flex-col items-center pt-4 dark:bg-gray-900">
+      <div class="flex w-full flex-col gap-4 md:max-w-screen-md">
+        <div class="flex flex-row justify-between">
+          <div></div>
+          <DifficultyButtons />
+        </div>
         <Board />
         <HighlightButtons />
       </div>
--- src/components/HighlightButtons.tsx
+++ src/components/HighlightButtons.tsx
@@ -5,13 +5,13 @@ import clsx from "clsx";
 function HighlightButtons() {
   const context = sudoku.state.context;
   return (
-    <div class="inline-flex select-none" role="group">
+    <div class="inline-flex w-full select-none" role="group">
       <For each={[1, 2, 3, 4, 5, 6, 7, 8, 9]}>
         {(num) => (
           <button
             type="button"
             class={clsx(
-              "border-t border-b border-r border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 first:rounded-l-lg last:rounded-r-lg hover:bg-gray-100 hover:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white",
+              "flex flex-grow border-t border-b border-r border-gray-200 bg-white py-2 text-center text-lg font-medium text-gray-900 first:rounded-l-lg last:rounded-r-lg hover:bg-gray-100 hover:text-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white",
               {
                 "z-10 text-blue-700 ring-2 ring-blue-700 dark:text-white dark:ring-blue-500":
                   context.highlight === num,
@@ -25,7 +25,7 @@ function HighlightButtons() {
               }
             }}
           >
-            {num}
+            <span class="mx-auto">{num}</span>
           </button>
         )}
       </For>
