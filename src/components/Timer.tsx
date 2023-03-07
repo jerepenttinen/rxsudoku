dc47decf0207696240465b746857c6e5dfd315a8
Jere Penttinen <jere@disroot.org>
2023-03-07 20:59:17 +0200

    Timer formatting

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,7 +3,7 @@ import { sudoku } from "../sudoku";
 export default function Timer() {
   const { timer } = sudoku.state.context;
   const display = () =>
-    new Date(timer.current - timer.started).toISOString().slice(11, 19);
+    new Date(timer.current - timer.started).toISOString().slice(14, 19);
 
   return (
     <div class="font-mono text-xl text-gray-800 dark:text-white">
