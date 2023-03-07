0420d3957e1006b8178d45539c3cb2e494e33c56
Jere Penttinen <jere@disroot.org>
2023-03-07 14:21:25 +0200

    Undo button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import withHotkeys from "./keyboard/withHotkeys";
 import Board from "./components/Board";
 import ActionButtons from "./components/ActionButtons";
 import DifficultyDropdown from "./components/DifficultyDropdown";
+import UndoButton from "./components/UndoButton";
 
 function App() {
   return (
     <div class="flex h-full min-h-screen flex-col items-center pt-4 dark:bg-gray-900">
       <div class="flex w-full flex-col gap-4 px-4 md:max-w-screen-sm md:px-0">
         <div class="flex flex-row justify-between">
-          <div></div>
+          <UndoButton />
           <DifficultyDropdown />
         </div>
         <Board />
